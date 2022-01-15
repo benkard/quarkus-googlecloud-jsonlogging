59942f5078eb782dbf09cd362bb2e2d1313aefdb
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2022-01-15 21:33:46 +0100

    Quarkus example: Use CDI instead of ServiceLoader for the provider.
    Change-Id: Ibbb012ea3fe9e32b8bc533724732885b57f87c88

--- examples/quarkus/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/example/RandomNumberParameterProvider.java
+++ examples/quarkus/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/example/RandomNumberParameterProvider.java
@@ -3,8 +3,12 @@ package eu.mulk.quarkus.googlecloud.jsonlogging.example;
 import eu.mulk.quarkus.googlecloud.jsonlogging.KeyValueParameter;
 import eu.mulk.quarkus.googlecloud.jsonlogging.StructuredParameter;
 import eu.mulk.quarkus.googlecloud.jsonlogging.StructuredParameterProvider;
+import io.quarkus.arc.Unremovable;
 import java.util.concurrent.ThreadLocalRandom;
+import javax.inject.Singleton;
 
+@Singleton
+@Unremovable
 public class RandomNumberParameterProvider implements StructuredParameterProvider {
 
   @Override
