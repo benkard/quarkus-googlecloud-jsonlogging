be4764837a880d193de52d42f428dfb46f0c7bf9
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2023-09-27 18:06:56 +0200

    fix(docs): Deal with TomcatURLStreamHandlerFactory conflict.
    Change-Id: Iab1bf00ad122464b8c770301d2c33406ece072e0

--- examples/spring-boot/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/example/Application.java
+++ examples/spring-boot/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/example/Application.java
@@ -4,11 +4,16 @@
 
 package eu.mulk.quarkus.googlecloud.jsonlogging.example;
 
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+  static {
+    TomcatURLStreamHandlerFactory.disable();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
