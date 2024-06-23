e9df29872c45ea24059c1c819325e38fe9abd89b
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2024-06-23 17:42:44 +0200

    Release 6.2.0.
    Change-Id: I06868ec47274e0fc2c01420d48c5e3d9f24ebeb6

--- runtime/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/runtime/package-info.java
+++ runtime/src/main/java/eu/mulk/quarkus/googlecloud/jsonlogging/runtime/package-info.java
@@ -43,7 +43,7 @@
  *     <dependency>
  *       <groupId>eu.mulk.quarkus-googlecloud-jsonlogging</groupId>
  *       <artifactId>quarkus-googlecloud-jsonlogging</artifactId>
- *       <version>6.1.0</version>
+ *       <version>6.2.0</version>
  *     </dependency>
  *
  *     ...
@@ -59,7 +59,7 @@
  * dependencies {
  *   // ...
  *
- *   implementation("eu.mulk.quarkus-googlecloud-jsonlogging:quarkus-googlecloud-jsonlogging:6.1.0")
+ *   implementation("eu.mulk.quarkus-googlecloud-jsonlogging:quarkus-googlecloud-jsonlogging:6.2.0")
  *
  *   // ...
  * }
